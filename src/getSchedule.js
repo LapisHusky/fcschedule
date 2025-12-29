e0beb047ced9a69e23ecdb888a7e8481411c7dff
Lapis <huskylapis@gmail.com>
2025-12-29 14:31:59 -0800

    use cors proxy for quicker schedule updates

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,5 +1,12 @@
 import schedule from "../schedule.json"
 
 export async function getSchedule() {
-  return schedule.entries
+  try {
+    let res = await fetch("https://fcschedulecorsproxy.lapishusky.dev/schedule_data/fc2026-schedule.json");
+    res = await res.json();
+    return res.entries;
+  } catch (error) {
+    console.error(error);
+    return schedule.entries;
+  }
 }
